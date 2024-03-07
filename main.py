0abd32f294e28433e81f1d159f2ab4a1a25fc7b0
raide <amirkhdaur@gmail.com>
2024-03-07 17:40:39 +0500

    fix

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(func=lambda message: True)
 def redirect_message(message):
+    print(message.text)
+    print(message.chat.id)
     if message.chat.id == config.FROM_CHAT_ID_1:
         bot.forward_message(from_chat_id=config.FROM_CHAT_ID_1, chat_id=config.TO_CHAT_ID,
                             message_thread_id=config.TO_MESSAGE_THREAD_ID_1, message_id=message.id)
